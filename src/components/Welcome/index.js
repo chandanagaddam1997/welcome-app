e96a1be4dbdad3a3cd2695ad638cb116b191c112
chandanadevigaddam <chandanagaddam77@gmail.com>
2023-03-08 02:24:42 +0000

    solution

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,31 +1,37 @@
 // Write your code here
 import {Component} from 'react'
+
 import './index.css'
 
 class Welcome extends Component {
-  state = {isLoggedIn: true}
+  state = {
+    isSubscribed: false,
+  }
+
+  onSubscribe = () => {
+    this.setState(prevState => ({isSubscribed: !prevState.isSubscribed}))
+  }
+
+  getButtonText = () => {
+    const {isSubscribed} = this.state
+
+    return isSubscribed ? 'Subscribed' : 'Subscribe'
+  }
 
   render() {
-    const {isLoggedIn} = this.state
-    let subscribeButton
-    if (isLoggedIn) {
-      subscribeButton = (
-        <button className="button" type="button">
-          Subscribed
-        </button>
-      )
-    } else {
-      subscribeButton = (
-        <button className="button" type="button">
-          Subscribe
-        </button>
-      )
-    }
+    const buttonText = this.getButtonText()
+
     return (
-      <div className="container">
+      <div className="app-container">
         <h1 className="heading">Welcome</h1>
-        <p className="paragraph">Thankyou!Happy Learning</p>
-        {subscribeButton}
+        <p className="description">Thank you! Happy Learning</p>
+        <button
+          type="button"
+          className="subscribe-button"
+          onClick={this.onSubscribe}
+        >
+          {buttonText}
+        </button>
       </div>
     )
   }
